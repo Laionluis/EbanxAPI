81d601af815ad06e9aa8716c86879fcc6806b1b1
Laionluis <llf15@inf.ufpr.br>
2022-03-09 23:54:19 -0300

    Adicionar arquivos de projeto.

---
+++ EbanxAPI/Controllers/AccountController.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Net.Http.Headers;
+using System.Web.Http;
+
+namespace EbanxAPI.Controllers
+{
+    public class AccountController : ApiController
+    {
+
+        public IEnumerable<string> Get()
+        {
+            return new string[] { ""};
+        }
+
+        // GET api/<controller>/5
+        public string Get(int id)
+        {
+            return "value";
+        }
+
+        // POST api/<controller>
+        public void Post([FromBody] string value)
+        {
+        }
+
+        // PUT api/<controller>/5
+        public void Put(int id, [FromBody] string value)
+        {
+        }
+
+        // DELETE api/<controller>/5
+        public void Delete(int id)
+        {
+        }
+
+        [Route("reset")]
+        public HttpResponseMessage ResetBalance()
+        {
+            var response = new HttpResponseMessage();
+            response.Content = new StringContent("OK");
+            response.Content.Headers.ContentType = new MediaTypeHeaderValue("text/html");
+            return response;
+        }
+    }
+}
---
+++ EbanxAPI/Startup.cs
@@ -0,0 +1,26 @@
+﻿using System.Web.Http;
+using Newtonsoft.Json.Serialization;
+using Owin;
+
+namespace EbanxAPI
+{
+    public class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            var config = new HttpConfiguration();
+
+            config.MapHttpAttributeRoutes();
+
+            config.Routes.MapHttpRoute(
+                name: "DefaultApi",
+                routeTemplate: "{controller}/{id}",
+                defaults: new { id = RouteParameter.Optional }
+            );
+
+            app.UseWebApi(config);
+
+           
+        }
+    }
+}
